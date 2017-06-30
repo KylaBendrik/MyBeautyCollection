2dd2fb7a744ce176ed7e272f33282ae0204ff41c
Beth Ashton <Beth@AshtonFam.org>
2017-06-30 19:47:49 -0400

    Functioning Dynamic Form
    three questions

--- main.js
+++ main.js
@@ -1,23 +1,26 @@
 domready(() => {
-  const article = document.querySelector('#mainArticle');
-
-  article.innerText = "This is your text now!";
-
-  const paragraph = document.createElement('p');
-
-  paragraph.innerText = 'This is a paragraph OF DOOM';
-
-  article.appendChild(paragraph);
-
-  console.log(article);
-
-  const thingInput = document.querySelector('input[name=product_type]:checked');
-
-  thingInput.addEventListener('input', _inputEvent => {
-    document.querySelector('#reporter').innerText = thingInput.value;
-  })
+  const thingInputs = document.querySelectorAll('input[name=product_type]');
+  const makeupInputs = document.querySelector('#makeupInputs');
+  const skincareInputs = document.querySelector('#skincareInputs');
 
 
+  const displayValue = function(value) {
+    if (value === 'makeup') {
+      makeupInputs.style.display = 'block';
+      skincareInputs.style.display = 'none';
+    } else if (value === 'skincare') {
+      makeupInputs.style.display = 'none';
+      skincareInputs.style.display = 'block';
+    }
+  };
+
+for (const thing of thingInputs){
+  thing.addEventListener('change', _inputEvent => {
+    displayValue(thing.value);
+  });
+}
+
+displayValue(document.querySelector ('input[name=product_type]:checked').value);
 
 
 });
