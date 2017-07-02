878c3d38061d6fe0de689c88457e00d1bf3567d6
Beth Ashton <Beth@AshtonFam.org>
2017-07-02 04:08:14 -0400

    Slightly Stuck - masks
    I think I've got it so selecting "facemask" (in skincareInputs) should display the maskInputs, but it's not working.
    Cannot read property 'value' of null
        at domready (main.js:44)

--- main.js
+++ main.js
@@ -1,10 +1,13 @@
 domready(() => {
-  const thingInputs = document.querySelectorAll('input[name=product_type]');
+  const productInputs = document.querySelectorAll('input[name=product_type]');
   const makeupInputs = document.querySelector('#makeupInputs');
   const skincareInputs = document.querySelector('#skincareInputs');
+  const skincareInputs1 = document.querySelectorAll('input[name=skincare_type]');
   const colorInputs = document.querySelector('#colorInputs');
+  const maskInputs = document.querySelector('#maskInputs');
   const hasColor = document.querySelector('#hasColor');
 
+//product type
   const displayValue = function(value) {
     if (value === 'makeup') {
       makeupInputs.style.display = 'block';
@@ -15,6 +18,32 @@ domready(() => {
     }
   };
 
+  for (const thing of productInputs){
+    thing.addEventListener('change', _inputEvent => {
+      displayValue(thing.value);
+    });
+  }
+
+  displayValue(document.querySelector ('input[name=product_type]:checked').value);
+
+//mask inputs
+  const displayValue1 = function(value) {
+    if (value === 'facemask') {
+      maskInputs.style.display = 'block';
+    } else {
+      maskInputs.style.display = 'none';
+    }
+  };
+
+  for (const thing of skincareInputs1){
+    thing.addEventListener('change', _inputEvent => {
+      displayValue1(thing.value);
+    });
+  }
+
+  displayValue1(document.querySelector ('input[name=skincare_type]:checked').value);
+
+//color inputs
 displayColor = function(checked) {
   if (checked){
     colorInputs.style.display = 'block';
@@ -29,13 +58,7 @@ hasColor.addEventListener('change', _changeEvent => {
 
 displayColor(hasColor.checked);
 
-for (const thing of thingInputs){
-  thing.addEventListener('change', _inputEvent => {
-    displayValue(thing.value);
-  });
-}
 
-displayValue(document.querySelector ('input[name=product_type]:checked').value);
 
 
 });
